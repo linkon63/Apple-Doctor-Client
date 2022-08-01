7e81e11f53ddd89013b6d8615667b85d253ab5ce
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2022-08-01 22:16:50 +0600

    blog added

--- src/components/Routes/ComponentsRoutes.jsx
+++ src/components/Routes/ComponentsRoutes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../Home/Home/Home'
+import Blog from '../Blog/Blog'
 const ComponentsRoutes = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="home" element={<Home />} />
+                <Route path="/blog" element={<Blog />} />
             </Routes>
         </div>
     );
