67232b910574a31454da9d4fe6489247f5f45662
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-08-02 00:12:30 +0600

    OrderService

--- src/components/Routes/ComponentsRoutes.jsx
+++ src/components/Routes/ComponentsRoutes.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from '../Home/Home/Home'
 import Blog from '../Blog/Blog'
+import OrderService from '../OrderService/OrderService/OrderService';
 const ComponentsRoutes = () => {
     return (
         <div>
@@ -9,6 +10,7 @@ const ComponentsRoutes = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="home" element={<Home />} />
                 <Route path="/blog" element={<Blog />} />
+                <Route path="/blog" element={<OrderService />} />
             </Routes>
         </div>
     );
